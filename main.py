50a58105b46c6f5f983b71498756d4262bddb82e
pydel <protoheo@gmail.com>
2022-11-08 15:20:02 +0900

    [2022.11.08 허영주]
    1. main.py 테스트

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class Thermometer:
+    def __init__(self, ):
+        driver = webdriver.Chrome('./drivers/chromedriver.exe')
+        driver.get('https://gagalive.com')
+
+        # driver.set_window_size(813, 512)
+        # driver.set_window_position(167, 354)
+        # driver.execute_script("window.scrollTo(0, 135)")
+
+        self.driver = driver
+
